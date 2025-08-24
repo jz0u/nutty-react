aa60a8e2f95f969268cf32dda8099e32fd0e6f20
Jackie Zou <jackiezou123@yahoo.com>
2025-08-24 12:37:37 -0400

    added hero component

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero(){
+
+    const headline = "Track food, hit goals, feel better."
+    const subheading = "Log meals in seconds and stay on track."
+    const callToAction=""
+    return(
+        <section id="hero-section">
+            <h1>{headline}</h1>
+            <h3>{subheading}</h3>
+            <button onClick={callToAction}>Get started!</button>
+        </section>
+    )
+}
