e66df481d5a022bf36599a23409b20b9aa7f8dad
Jackie Zou <jackiezou123@yahoo.com>
2025-08-27 16:33:01 -0400

    added filter to api/user

--- server/index.js
+++ server/index.js
@@ -1,33 +1,46 @@
-import express from "express";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import path from "path"
+import { fileURLToPath } from "url"
 
-dotenv.config();
+//db
+import users from "./users.js"
+import { send } from "process"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const reactDist = path.join(__dirname, "../client/dist")
 
-const app = express();
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.static(reactDist))
 
-const PORT = process.env.PORT || 3000;
-
-// path to React app (Vite default)
-const clientDir = path.join(__dirname, "../client/dist");
-
-// serve static assets
-app.use(express.static(clientDir));
-
-// put API routes BEFORE the SPA fallback
-// app.get("/api/health", (_req, res) => res.json({ ok: true }));
-
-// SPA fallback: send index.html for any non-API route
-app.use((req, res, next) => {
-  // let real API routes fall through if you add them later
-  if (req.path.startsWith("/api")) return next();
-  res.sendFile(path.join(clientDir, "index.html"));
-});
-
+//endpoints
+app.get("/api", (req, res) => {
+  res.sendStatus(200)
+})
+app.get("/api/users", (req, res) => {
+ const {query:{filter,value}} = req
+ if(!filter||!value){return res.send(users)}
+ res.send(
+  users.filter((x)=>x[filter].includes(value))
+)
+})
+app.get("/api/users/:id", (req, res) => {
+  const parsedId = parseInt(req.params.id)
+  if(isNaN(parsedId)){return res.status(400).send({msg: "Bad Request"})}
+  const findUser = users.find((x)=>(x.id === parsedId))
+  if(!findUser){return res.status(404).send({msg:"User Not Found"})}
+  return res.status(200).json(findUser)
+})
+//SPA catch all goes last
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(reactDist, "index.html"))
+})
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  console.log(`\nlistening on http://localhost:${PORT}\n`)
+})
+
+
---
+++ server/users.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        id:1,
+        username:"jackie",
+    },
+    {
+        id:2,
+        username:"jacy"
+    }
+]
